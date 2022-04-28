b407e424e0abe37a78b643b648958ebcfea59656
Rodrigo737 <80003316+Rodrigo737@users.noreply.github.com>
2022-04-28 12:55:08 -0400

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -142,7 +142,7 @@ function createPie(tableData){
     };  
 }
 function createBar(tableData){     
-    let selected = $("#pieSlider").val();     //logic: gets difference of year and applies it to the column to get the right column for the selected year
+    let selected = $("#barSlider").val();     //logic: gets difference of year and applies it to the column to get the right column for the selected year
     let yearColumn = 120 - (2020 - selected);  
     let year = tableData[0][yearColumn];
     let barData = [];
